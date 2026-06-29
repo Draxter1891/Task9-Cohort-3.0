0bde51245d7d2fcf535b980db10337c5ff7e8fc9
Rishabh Tripathi <trishabh2001@gmail.com>
2026-06-29 21:41:45 +0530

    Features added: Theme changer, dynamic UI rendering of Dashboard Bug fixes: Dark mode theme persistence

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let users = JSON.parse(localStorage.getItem("users")) || [
     email: "rishabh@gmail.com",
     password: "123456",
     currency: "INR",
-    darkMode: false,
+    darkMode: true,
 
     transactions: [
       {
@@ -18,15 +18,62 @@ let users = JSON.parse(localStorage.getItem("users")) || [
         category: "Housing",
         date: "2026-06-30",
       },
+      {
+        id: 101,
+        type: "income",
+        description: "salary",
+        amount: 18000,
+        category: "Housing",
+        date: "2026-06-30",
+      },
+      {
+        id: 101,
+        type: "income",
+        description: "salary",
+        amount: 18000,
+        category: "Housing",
+        date: "2026-06-30",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Radhika",
+    email: "radhika@gmail.com",
+    password: "123456",
+    currency: "INR",
+    darkMode: false,
+
+    transactions: [
+      {
+        id: 1001,
+        type: "income",
+        description: "salary",
+        amount: 80000,
+        category: "Housing",
+        date: "2026-06-30",
+      },
     ],
   },
 ];
+
+
 const currentUser = users[0];
 
-let formOverlay = document.querySelector(".form-overlay");
-let addTransaction = document.querySelector("#add-transation");
-let formClose = document.querySelector(".close-btn");
-let form = document.querySelector("form");
+let chart = null;
+
+//DOM elements
+const userName = document.querySelector("#user-name");
+const formOverlay = document.querySelector(".form-overlay");
+const addTransaction = document.querySelector("#add-transation");
+const formClose = document.querySelector(".close-btn");
+const form = document.querySelector("form");
+const darkMode = document.querySelector("#dark");
+const chartCanvas = document.querySelector("#cashFlowChart");
+const balanceCard = document.querySelector("#balance");
+const incomeCard = document.querySelector("#income");
+const expenseCard = document.querySelector("#expense");
+const transactionsCard = document.querySelector("#transactions");
 
 //form data
 const type = document.querySelector("#type");
@@ -34,6 +81,18 @@ const description = document.querySelector("#description");
 const amount = document.querySelector("#amount");
 const date = document.querySelector("#date");
 const category = document.querySelector("#category");
+
+//Event listeners
+
+darkMode.addEventListener("change", (e) => {
+  const isDarkMode = e.target.checked;
+
+  currentUser.darkMode = isDarkMode;
+  applyTheme(isDarkMode);
+  saveUsers()
+  console.log(getUser())
+});
+
 
 addTransaction.addEventListener("click", () => {
   formOverlay.style.display = "block";
@@ -51,7 +110,6 @@ form.addEventListener("submit", (e) => {
     date.value === "" ||
     category.value === ""
   ) {
-    formOverlay.style.display = "none";
     alert("please fill all the fields");
     return;
   }
@@ -68,11 +126,90 @@ form.addEventListener("submit", (e) => {
   currentUser.transactions.push(transaction);
 
   saveUsers();
-
+  getTotals(users);
+  renderUI();
   form.reset();
   formOverlay.style.display = "none";
 });
 
+function renderUI() {
+  let loggedinUser = getUser();
+
+  applyTheme(loggedinUser.darkMode);
+
+  userName.textContent = `${loggedinUser.name}`;
+
+  let totals = getTotals(loggedinUser);
+  balanceCard.textContent = totals[0] - totals[1];
+  incomeCard.textContent = totals[0];
+  expenseCard.textContent = totals[1];
+  transactionsCard.textContent = loggedinUser.transactions.length;
+
+  renderChart(totals[0], totals[1]);
+}
+
+renderUI();
+
+
+
+//Utility functions
+
+function applyTheme(isDarkMode) {
+  if (isDarkMode) {
+    document.body.setAttribute("data-theme", "dark");
+    darkMode.checked = true
+} else {
+    document.body.removeAttribute("data-theme");
+    darkMode.checked = false
+  }
+}
+
+function getTotals(user) {
+  let totalExpense = 0,
+    totalIncome = 0;
+  //   let loggedinUser = users.find((user) => user.id === currentUser.id);
+  let money = user.transactions;
+  money.forEach((element) => {
+    if (element.type === "expense") {
+      totalExpense += element.amount;
+    } else if (element.type === "income") {
+      totalIncome += element.amount;
+    }
+  });
+  return [totalIncome, totalExpense];
+}
+
+function renderChart(totalIncome, totalExpense) {
+  new Chart(chartCanvas, {
+    type: "bar",
+
+    data: {
+      labels: ["Income vs expense"],
+      datasets: [
+        {
+          label: "Income",
+          data: [totalIncome],
+          borderColor: "#01792d",
+          backgroundColor: "#16a34a",
+        },
+        {
+          label: "expense",
+          data: [totalExpense],
+          borderColor: "#b60a2f",
+          backgroundColor: "#dc2626",
+        },
+      ],
+    },
+  });
+}
+
 function saveUsers() {
   localStorage.setItem("users", JSON.stringify(users));
 }
+function getUser() {
+  let allUsers = JSON.parse(localStorage.getItem("users"));
+  let loggedinUser = allUsers.find((user) => user.id === currentUser.id);
+  return loggedinUser;
+}
+
+console.log(getUser());
